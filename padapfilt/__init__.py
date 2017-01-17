b27d1efea53ed039aa59c2bf2d6b708233878a3f
zekeriyasari <zekeriya.sari.deu@gmail.com>
2017-01-17 10:31:28 +0300

    fixed the __init__.py files

--- padapfilt/__init__.py
+++ padapfilt/__init__.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-=======
+
 
 from padapfilt.filters import *
 from padapfilt.constants import *
 from padapfilt.utils import *
 
->>>>>>> beebc67f8600e5340572bb3d339b5d52c2ad9a17
+
--- plotting.py
+++ plotting.py
@@ -11,14 +11,6 @@ def get_learning_curve_plot():
 
 
 def get_tap_weights_graph(n):
-<<<<<<< HEAD
-    fig, ax = plt.subplots(n)
-    for i in range(n):
-        ax[i].set_xlabel(r'$k$')
-        ax[i].set_ylabel(r'$\hat{w}_k$')
-        ax[i].set_ylim([-2, 2])
-    plt.tight_layout()
-=======
     if n:
         if n > 1:
             fig, ax = plt.subplots(n)
@@ -36,5 +28,4 @@ def get_tap_weights_graph(n):
         ax.set_xlabel(r'$k$')
         ax.set_ylabel(r'$\hat{w_a}_k$')
         ax.set_ylim([-2, 2])
->>>>>>> beebc67f8600e5340572bb3d339b5d52c2ad9a17
     return fig, ax
